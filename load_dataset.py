7f4430675ab8daa0420e62b5f9af411a6762107e
rozoomcool <rozoom.um@gmail.com>
2023-11-11 19:50:19 +0300

    '.'

--- load_dataset.py
+++ load_dataset.py
@@ -2,82 +2,65 @@ import os
 
 import numpy as np
 import torch
-from PIL import Image
 from torch.utils.data import Dataset
 import cv2
 from torchvision import transforms as T
-import torchvision.transforms as transforms
+from PIL import Image
 
 
 class AVADataset(Dataset):
-    def __init__(self, root_directory, transform=None):
-        """
-        Инициализация датасета.
-        :param videos_root: Корневая директория с папками видео, где каждая папка соответствует классу.
-        :param transform: Опциональные трансформации, которые нужно применить к кадрам видео.
-        """
-        self.root_directory = root_directory
-        self.videos_root = f'{root_directory}videos'
-        self.transform = transform
-        # self.class_to_idx = pd.read_csv(f'{root_directory}classes.csv')
-        # print(self.class_to_idx)
+    def __init__(self, root_directory, frames_len=16):
+        self.videos_root = os.path.join(root_directory, 'videos')
+        self.frames_len = frames_len
         self.classes = os.listdir(self.videos_root)
-        # self.class_to_idx = {cls_name: idx for idx, cls_name in enumerate(self.classes)}
         self.videos = self._load_videos()
 
+        # Define the transformations
         self.transform = T.Compose([
-            # Ваши трансформации здесь, например:
             T.Resize((128, 128)),
             T.ToTensor(),
-            T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+            T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ])
 
     def _load_videos(self):
         videos = []
         for class_idx, class_name in enumerate(self.classes):
             class_dir = os.path.join(self.videos_root, class_name)
-            for video_name in os.listdir(class_dir):
-                print(video_name)
+            for video_name in os.listdir(class_dir):  # Make sure to use the correct video file extension
                 videos.append((os.path.join(class_dir, video_name), class_idx))
-        return videos
+        return list(set(videos))
 
     def __len__(self):
         return len(self.videos)
 
     def __getitem__(self, idx):
         video_path, class_idx = self.videos[idx]
-        # Загрузите ваше видео и преобразуйте его в последовательность кадров
-        # Это может быть ресурсоемкая операция, в зависимости от размера и длительности видео
         frames = self._load_video_frames(video_path)
 
-        if self.transform:
-            frames = [self.transform(frame) for frame in frames]
+        if len(frames) < self.frames_len:
+            frames += [frames[-1]] * (self.frames_len - len(frames))
+        frames = frames[:self.frames_len]
 
-        # Переведите список трансформированных кадров в тензор
-        frames_tensor = torch.stack(frames)
+        # Transform each frame
+        frames_tensor = torch.stack([self.transform(frame) for frame in frames], dim=0)
+        frames_tensor = frames_tensor.permute(1, 0, 2, 3)
         return frames_tensor, class_idx
 
     def _load_video_frames(self, video_path):
         cap = cv2.VideoCapture(video_path)
-        clips = []
-        while cap.isOpened():
+        frames = []
+        for _ in range(self.frames_len):
             ret, frame = cap.read()
             if not ret:
                 break
-            # Преобразуем кадр из BGR в RGB и создаем PIL Image
+
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             frame = Image.fromarray(frame)
-
-            # Применяем трансформации, ожидающие PIL Images или ndarrays
-            if self.transform:
-                frame = self.transform(frame)
-
-            # Добавляем преобразованный кадр в список
-            clips.append(frame)
-
+            frames.append(frame)
         cap.release()
 
-        # Преобразуем список PIL Images или ndarrays в тензоры
-        clips_tensor = torch.stack([transforms.ToTensor()(frame) for frame in clips])
+        # If there were not enough frames, pad with the last frame
+        while len(frames) < self.frames_len:
+            frames.append(frames[-1] if frames else Image.new('RGB', (128, 128), color=(0, 0, 0)))
 
-        return clips_tensor
+        return frames
--- main.py
+++ main.py
@@ -1,9 +1,63 @@
-from finetune import execute_finetune
+import torch
+import torchvision.models as models
+from torch.utils.data import DataLoader
+
+from load_dataset import AVADataset
 
 
-def main():
-    execute_finetune()
+def execute_finetune():
+    root_dir = 'C:/Users/adam/Pictures/ttt'
+    # root_dir = 'C:/Users/adam/Pictures/rjd/rjdcv/train/train/'
+
+    num_classes = 24  # Update this to the number of classes in your dataset
+    batch_size = 16
+    learning_rate = 0.001
+    num_epochs = 2
+
+    # Initialize the dataset
+    train_dataset = AVADataset(root_directory=root_dir)
+    train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+
+    # Initialize the model
+    model = models.video.r3d_18()
+    num_ftrs = model.fc.in_features
+    model.fc = torch.nn.Linear(num_ftrs, num_classes)
+
+    # If using a GPU
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = model.to(device)
+    print("Start on cuda" if torch.cuda.is_available() else " Start on cpu")
+    print(device)
+
+    # Loss and optimizer
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+    # Training loop
+    for epoch in range(num_epochs):
+        print(f'START_EXECUTION: Epoch [{epoch + 1}/{num_epochs}]')
+        model.train()
+        running_loss = 0.0
+        for images, labels in train_loader:
+            images = images.to(device)
+            labels = labels.to(device)
+
+            # Forward pass
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+
+            # Backward pass and optimization
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+
+        print(f'Epoch [{epoch + 1}/{num_epochs}], Loss: {running_loss / len(train_loader):.4f}')
+
+    # Save the model checkpoint
+    torch.save(model.state_dict(), 'model.pth')
 
 
 if __name__ == '__main__':
-    main()
+    execute_finetune()
